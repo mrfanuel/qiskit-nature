6a28dbf167c10e6a64b9a41d8acbdd3946ac3ad0
Michaël Fanuel <mrfanuel@hotmail.fr>
2023-04-07 13:47:04 +0200

    notice of modification

--- qiskit_nature/second_q/circuit/library/initial_states/utils/givens_rotations.py
+++ qiskit_nature/second_q/circuit/library/initial_states/utils/givens_rotations.py
@@ -10,6 +10,8 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+# /!\ this code has been modified !
+
 """Private helper functions for initial states."""
 
 from __future__ import annotations
