ce908a727e494d11c6fe325351b196fc61b06ac1
Michaël Fanuel <mrfanuel@hotmail.fr>
2023-05-16 10:50:58 +0200

    refactored to minimize code duplication

--- qiskit_nature/second_q/circuit/library/initial_states/utils/givens_rotations.py
+++ qiskit_nature/second_q/circuit/library/initial_states/utils/givens_rotations.py
@@ -11,6 +11,9 @@
 # that they have been altered from the originals.
 
 # /!\ this code has been modified !
+# most of the content of _prepare_slater_determinant_jw has been put into 
+# _gates_from_transformation_matrix, so that it can be reused into 
+# _prepare_fermionic_gaussian_state_jw
 
 """Private helper functions for initial states."""
 
@@ -39,6 +42,27 @@ def _prepare_slater_determinant_jw(
     Yields:
         (gate, qubits) pairs describing the operations, where the qubits
         are provided in a tuple
+    """
+    decomposition_reversed = _gates_from_transformation_matrix(
+    register, transformation_matrix)
+
+    yield from decomposition_reversed
+
+def _gates_from_transformation_matrix(
+    register: QuantumRegister, transformation_matrix: np.ndarray
+) -> Iterable[Tuple[Gate, Tuple[Qubit, ...]]]:
+    """Decomposes transformation matrix V as a sequence of Givens rotations
+    acting on specific columns.
+
+    Args:
+        register: The register containing the qubits to use
+        transformation_matrix: The transformation matrix describing
+            the Slater determinant.
+
+    Yields:
+        (gate, qubits) pairs describing the operations in the order they have to appear in the circuit, where the qubits
+        are provided in a tuple.
+        Note that the corresponding gates in the reverse order wrt to the decomposition of V.
     """
     m, n = transformation_matrix.shape
 
@@ -81,9 +105,6 @@ def _prepare_slater_determinant_jw(
                 )
 
     yield from reversed(decomposition)
-
-
-    
     
 def _prepare_fermionic_gaussian_state_jw(  # pylint: disable=invalid-name
     register: QuantumRegister, transformation_matrix: np.ndarray, occupied_orbitals: Sequence[int]
@@ -105,53 +126,14 @@ def _prepare_fermionic_gaussian_state_jw(  # pylint: disable=invalid-name
     right_decomposition, left_unitary = fermionic_gaussian_decomposition_jw(
         register, transformation_matrix
     )
+    right_decomposition_reversed = reversed(right_decomposition)
     
     # decompose the left unitary matrix (V^T)_{C :}
     # as a product of Givens only.
-    # In what follows: simply a copy-paste of _prepare_slater_determinant_jw
-    # /!\ to be refactored to avoid redundant code.
     left_transformation_matrix = left_unitary.T[list(occupied_orbitals)]
+    left_decomposition_reversed = _gates_from_transformation_matrix(register,left_transformation_matrix)
     
-    m, n = left_transformation_matrix.shape
-
-    # set the first n_particles qubits to 1
-    for i in range(m):
-        yield XGate(), (register[i],)
-
-    # if all orbitals are filled, no further operations are needed
-    if m == n:
-        return
-
-    current_matrix = left_transformation_matrix
-
-    # zero out top right corner by rotating rows; this is a no-op
-    for j in reversed(range(n - m + 1, n)):
-        # Zero out entries in column j
-        for i in range(m - n + j):
-            # Zero out entry in row i if needed
-            if not np.isclose(current_matrix[i, j], 0.0):
-                givens_mat = givens_matrix(current_matrix[i + 1, j], current_matrix[i, j])
-                current_matrix = apply_matrix_to_slices(current_matrix, givens_mat, [i + 1, i])
-
-    # decompose matrix into Givens rotations
-    left_decomposition: List[Tuple[Gate, Tuple[Qubit, ...]]] = []
-    for i in range(m):
-        # zero out the columns in row i
-        for j in range(n - m + i, i, -1):
-            if not np.isclose(current_matrix[i, j], 0.0):
-                # compute Givens rotation
-                givens_mat = givens_matrix(current_matrix[i, j - 1], current_matrix[i, j])
-                theta = np.arccos(np.real(givens_mat[0, 0]))
-                phi = np.angle(givens_mat[0, 1])
-                # add operations
-                left_decomposition.append(
-                    (XXPlusYYGate(2 * theta, phi - np.pi / 2), (register[j], register[j - 1]))
-                )
-                # update matrix
-                current_matrix = apply_matrix_to_slices(
-                    current_matrix, givens_mat, [(Ellipsis, j - 1), (Ellipsis, j)]
-                )
-    # output gates in reverse order
-    yield from reversed(left_decomposition)
-    yield from reversed(right_decomposition)
+    # output gates in reverse order 
+    yield from left_decomposition_reversed
+    yield from right_decomposition_reversed
   
